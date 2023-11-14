456b8e4329e141379321aeec0a484c4038aa5074
yashcrest <yashcrest@gmail.com>
2023-11-14 19:53:41 +1030

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+//this is vanilla nodeJS
+
+const port = 3000;
+console.log(process.platform);
+console.log("hello world!")
+
+const http = require('http');
+const { hostname } = require('os');
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text/plain');
+    res.end('Hello world\n');
+})
+
+server.listen((port, hostname, () => {
+    console.log(`server running on http://${hostname}/${port}/`);
+}))
